86eda2a7989929ed51b79b63a0326f033e01dd2c
riyaz ahmad <riyaz.ahmad@indusnet.co.in>
2023-04-03 00:23:28 +0530

    wokring partially

--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -1,27 +1,42 @@
 import { Button, Input } from "@chakra-ui/react";
 import { useState } from "react";
-import "./InputForm.css"
+import "./InputForm.css";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const InputForm = () => {
-
-    const navigate= useNavigate()
-    let flag= false;
+  const navigate = useNavigate();
+  let flag = false;
   const [seatValue, setSeatValue] = useState();
 
   const onHandleClick = () => {
-    console.log({flag})
-    if(seatValue>0 && seatValue<=7)
-    {
-        navigate("/seatpage")
-        localStorage.setItem("seatNumberInput",seatValue)
-    }
+    console.log({ flag });
+    if (seatValue > 0 && seatValue <= 7) {
+      let arr = [];
+      axios
+        .get("http://localhost:2000/get_data")
+        .then((res) => {
+          arr = res.data.data;
 
+          for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr[i].length; j++) {
+              arr[i][j].justBooked = false;
+            }
+          }
+
+          localStorage.setItem("data", JSON.stringify(arr));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      navigate("/seatpage");
+
+      localStorage.setItem("seatNumberInput", seatValue);
+    }
   };
   const onHandleChange = (e) => {
     setSeatValue(e.target.value);
-
-    
   };
 
   return (
@@ -33,7 +48,9 @@ export const InputForm = () => {
         placeholder="number of seats"
         onChange={onHandleChange}
       />
-      {seatValue>7 || seatValue==0? <p>*please enter a value between 1 to 7</p>:null}
+      {seatValue > 7 || seatValue == 0 ? (
+        <p>*please enter a value between 1 to 7</p>
+      ) : null}
       <Button mt={5} type="submit" onClick={onHandleClick}>
         {" "}
         Book
--- src/SeatPage.jsx
+++ src/SeatPage.jsx
@@ -1,116 +1,147 @@
+import { useEffect } from "react";
+import "./SeatPage.css";
+import axios from "axios";
+export const SeatPage = () => {
+  // let arr=[[1,2,3,4,5,6,7],[8,9,10,11,12,13,14],[15,16,17,18,19,20,21],[22,23,24,25,26,27,28],[29,30,31,32,33,34,35],[36,37,38,39,40,41,42],[43,44,45,46,47,48,49],[50,51,52,53,54,55,56],[57,58,59,60,61,62,63],[64,65,66,67,68,69,70],[71,72,73,74,75,76,77],[78,79,80]]
 
-import "./SeatPage.css"
-export const SeatPage=()=>{
+  // let arr=[]
+  // let i=1
+  // while(i<=80)
+  // {
+  //     let a1=[]
+  //     let temp=i
+  //     for(i; i<temp+7; i++)
+  //     {
+  //         if(i>80)
+  //         {
+  //             break
+  //         }
+  //         let obj={seatNumber:i,bookingStatus:"empty",justBooked:false}
+  //         a1.push(obj)
+  //     }
 
-  
-    // let arr=[[1,2,3,4,5,6,7],[8,9,10,11,12,13,14],[15,16,17,18,19,20,21],[22,23,24,25,26,27,28],[29,30,31,32,33,34,35],[36,37,38,39,40,41,42],[43,44,45,46,47,48,49],[50,51,52,53,54,55,56],[57,58,59,60,61,62,63],[64,65,66,67,68,69,70],[71,72,73,74,75,76,77],[78,79,80]]
+  //     arr.push(a1)
+  // }
 
-    let arr=[]
-    let i=1
-    while(i<=80)
-    {
-        let a1=[]
-        let temp=i
-        for(i; i<temp+7; i++)
-        {
-            let obj={seatNumber:i,bookingStatus:"empty",justBooked:false}
-            a1.push(obj)
-        }
-        
-        arr.push(a1)
+  // arr[0][0].bookingStatus="fill"
+  // console.log(arr)
+  let arr = localStorage.getItem("data");
+
+//   console.log(arr);
+
+  arr = JSON.parse(arr);
+
+//   for(let i=0; i<arr.length;  i++)
+//   {
+//     for(let j=0;  j<arr[i].length; j++)
+//     {
+//         arr[i][j].justBooked=false
+//     }
+//   }
+
+console.log("checking",{arr})
+
+  let numberOfSeatToBook = localStorage.getItem("seatNumberInput");
+
+  for (let i = 0; i < arr.length; i++) {
+    let bookedStatus = false;
+    let emptyCount = 0;
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j].bookingStatus == "empty") {
+        emptyCount++;
+      }
+      if (emptyCount == numberOfSeatToBook) {
+        break;
+      }
     }
-
-    arr[0][0].bookingStatus="fill"
-
-    let numberOfSeatToBook= localStorage.getItem("seatNumberInput")
-
-
-    for(let i=0; i<arr.length; i++)
-    {
-        let bookedStatus=false
-        let emptyCount=0
-        for(let j=0; j<arr[i].length; j++)
-        {
-             if(arr[i][j].bookingStatus=="empty")
-             {
-                emptyCount++
-             }
-             if(emptyCount==numberOfSeatToBook)
-             {
-                break;
-             }
+    if (emptyCount == numberOfSeatToBook) {
+      let fillCount = 0;
+      for (let j = 0; j < arr[i].length; j++) {
+        if (arr[i][j].bookingStatus == "empty") {
+          arr[i][j].bookingStatus = "fill";
+          arr[i][j].justBooked = true;
+          fillCount++;
         }
-        if(emptyCount==numberOfSeatToBook)
-        {
-            let fillCount=0
-            for(let j=0; j<arr[i].length; j++)
-            {
-                if(arr[i][j].bookingStatus=="empty")
-                {
-                    arr[i][j].bookingStatus="fill";
-                    arr[i][j].justBooked=true
-                    fillCount++
-                    
-                }
-                if(fillCount==numberOfSeatToBook)
-                {
-                    break;
-                }
-            }
+        if (fillCount == numberOfSeatToBook) {
 
-            if(fillCount==numberOfSeatToBook)
-            {
-                break
-            }
+            bookedStatus=true
+          break;
         }
+      }
+
     }
-    console.log({arr})
+    if (bookedStatus==true) {
+      break;
+    }
+  }
+arr=JSON.stringify(arr)
 
 
+// localStorage.setItem("data", arr)
 
-    
+// arr=localStorage.getItem("data")
+console.log(arr)
+      axios.post("http://localhost:2000/book_seats",{
+        data: arr
+      }).then((res)=>{
+        console.log({res})
+      })
 
-    return(
-        <>
-        <div>
-            <h3>booking page</h3>
+arr=JSON.parse(arr)
 
-            <div className="box">
+  return (
+    <>
+      <div>
+        <h3>booking page</h3>
 
-                <div className="flax-box">
-                    {arr.map((e)=>{
-                        return(
-                            <>
-                            <div className="row">
-                                {e.map((k)=>{
-                                    
-                                        if(k.bookingStatus=="empty")
-                                        {
-                                        return (<button> {k.seatNumber} </button>)
-                                        }
-                                         else if(k.bookingStatus=="fill" && k.justBooked==true){
-                                            return(<button style={{ border:"2px solid green", color:"white" ,backgroundColor:"green"}}> {k.seatNumber}</button>) 
-
-                                        }
-                                        else{
-                                            return(
-                                                <button style={{border:"2px solid gray", color:"white",backgroundColor:"gray"}}> {k.seatNumber}</button>
-                                            )
-                                        }
-                                        
-                                    
-                                })}
-                            </div>
-                            <hr />
-                            </>
-                        )
+        <div className="box">
+          <div className="flax-box">
+            {arr.map((e) => {
+              return (
+                <>
+                  <div className="row">
+                    {e.map((k) => {
+                      if (k.bookingStatus == "empty") {
+                        return <button> {k.seatNumber} </button>;
+                      } else if (
+                        k.bookingStatus == "fill" &&
+                        k.justBooked == true
+                      ) {
+                        return (
+                          <button
+                            style={{
+                              border: "2px solid green",
+                              color: "white",
+                              backgroundColor: "green",
+                            }}
+                          >
+                            {" "}
+                            {k.seatNumber}
+                          </button>
+                        );
+                      } else {
+                        return (
+                          <button
+                            style={{
+                              border: "2px solid gray",
+                              color: "white",
+                              backgroundColor: "gray",
+                            }}
+                          >
+                            {" "}
+                            {k.seatNumber}
+                          </button>
+                        );
+                      }
                     })}
-                </div>
-
-            </div>
+                  </div>
+                  <hr />
+                </>
+              );
+            })}
+          </div>
         </div>
-       
-        </>
-    )
-
-}
+      </div>
+    </>
+  );
+};
